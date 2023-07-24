9fad376851bfcbb1330509a4ac4c37440185f82a
arts1989 <arts1989@bk.ru>
2023-07-24 14:52:45 +0300

    фикс по окнам, чтобы одновременно не вылезали

--- Assets/Scripts/Systems/UpdateScoreWidgetSystem.cs
+++ Assets/Scripts/Systems/UpdateScoreWidgetSystem.cs
@@ -21,18 +21,17 @@ namespace Match3
             {
                 _gameState.MovesAvaliable--;
                 
-                _sceneData.UI.scoreWidget.SetMovesLeftText(_gameState.MovesAvaliable);
-                if (_gameState.MovesAvaliable == 0)
-                {
-                    _filter.GetEntity(0).Get<LoseEvent>();
-                }
-
                 _sceneData.UI.scoreWidget.SetPointsScoredText(_gameState.PointsScored);
                 if(_gameState.PointsScored > _gameState.PointsToWin)
                 {
                     _filter.GetEntity(0).Get<WinEvent>();
                 }
 
+                _sceneData.UI.scoreWidget.SetMovesLeftText(_gameState.MovesAvaliable);
+                if (_gameState.MovesAvaliable == 0 && _gameState.PointsScored < _gameState.PointsToWin) // fix иначе может быть и вин и луз окна
+                {
+                    _filter.GetEntity(0).Get<LoseEvent>();
+                }
 
                 _filter.GetEntity(0).Del<UpdateScoreEvent>(); //destroy entity with UpdateScoreEvent component
             }
