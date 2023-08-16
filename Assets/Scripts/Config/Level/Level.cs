37d0acec639d0d42c152518a3f13d0460198d837
arts1989 <arts1989@bk.ru>
2023-08-16 20:52:11 +0300

    fix

--- Assets/Scripts/Config/Level/Level.cs
+++ Assets/Scripts/Config/Level/Level.cs
@@ -10,16 +10,17 @@ namespace Match3
 	public class Level : ScriptableObject
 	{
 		public int PointsToWin = 100;
-		
+
 		public int Columns = 5;
 		public int Rows = 5;
 
 		public int MovesAvailable = 5;
-    
-    public int ObstacleCount = 0;
+
+		public int ObstacleCount = 0;
 		public int UnderlayCount = 0;
-	
+
 		[Header("Board properties")]
 		public List<ItemsDictionaryService<BlockTypes, Vector2Int>> blocksProperties;
 		public List<ItemsDictionaryService<BlockTypes, Vector2Int>> underlaysProperties;
+	}	
 }
