9c96ab451a2d73479bd60422c31be7a47b2ec5f1
mityus <mityushinaworking@gmail.com>
2023-08-18 21:41:21 +0300

    add new property level

--- Assets/Scripts/Config/Level/Level.cs
+++ Assets/Scripts/Config/Level/Level.cs
@@ -19,5 +19,10 @@ namespace Match3
 		[Header("Blocks and underlays properties")]
 		public List<SerializeItem<Vector2Int,BlockTypes>> blocksProperties;
 		public List<SerializeItem<Vector3Int,UnderlayTypes>> underlaysProperties;
+
+		[Space] 
+		[SerializeField] private List<Vector2Int> precipiceProperty;
+
+		public IReadOnlyList<Vector2Int> PrecipiceProperty => precipiceProperty;
 	}	
 }
